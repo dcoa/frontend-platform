d0334e10f8b75953b57a0c7dc0e9cc2df6311aa4
Diana Catalina Olarte <diana.olarte@edunext.co>
2023-11-30 15:05:08 +1100

    ver 2.6.2 with css

--- react/hooks/useAppEvent.js
+++ react/hooks/useAppEvent.js
@@ -1,6 +1,6 @@
-/* eslint-disable import/prefer-default-export */
 import { useEffect } from 'react';
-import { subscribe, unsubscribe } from '../pubSub';
+import { subscribe, unsubscribe } from '../../pubSub';
+
 /**
  * A React hook that allows functional components to subscribe to application events.  This should
  * be used sparingly - for the most part, Context should be used higher-up in the application to
@@ -8,16 +8,17 @@ import { subscribe, unsubscribe } from '../pubSub';
  * mechanism.
  *
  * @memberof module:React
+ *
  * @param {string} type
  * @param {function} callback
  */
-
-export var useAppEvent = function useAppEvent(type, callback) {
+var useAppEvent = function useAppEvent(type, callback) {
   useEffect(function () {
     var subscriptionToken = subscribe(type, callback);
     return function cleanup() {
       unsubscribe(subscriptionToken);
     };
-  }, []);
+  }, [callback, type]);
 };
-//# sourceMappingURL=hooks.js.map
+export default useAppEvent;
+//# sourceMappingURL=useAppEvent.js.map
